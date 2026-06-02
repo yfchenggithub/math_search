94250e20cd45c44ead908719eaba3c84dbc2ffb5
yfcheng <18912964525@163.com>
2026-06-02 16:27:36 +0800

    搜索框公共组件

---
+++ miniprogram/components/search-box/search-box.ts
@@ -0,0 +1,50 @@
+type SearchBoxInputEvent = {
+  detail: {
+    value?: string;
+  };
+};
+
+Component({
+  properties: {
+    value: {
+      type: String,
+      value: "",
+    },
+    placeholder: {
+      type: String,
+      value: "",
+    },
+    confirmType: {
+      type: String,
+      value: "search",
+    },
+    focus: {
+      type: Boolean,
+      value: false,
+    },
+    showClear: {
+      type: Boolean,
+      value: false,
+    },
+    spacingTop: {
+      type: Boolean,
+      value: false,
+    },
+  },
+
+  methods: {
+    handleInput(event: SearchBoxInputEvent) {
+      this.triggerEvent("input", {
+        value: event.detail.value || "",
+      });
+    },
+
+    handleConfirm() {
+      this.triggerEvent("confirm");
+    },
+
+    handleClear() {
+      this.triggerEvent("clear");
+    },
+  },
+});
